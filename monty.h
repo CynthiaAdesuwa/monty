a9d467207887233d5c0489b504d8d849990b43e0
CynthiaAdesuwa <cynthiaadesuwaosagie@gmail.com>
2022-12-14 05:15:27 -0800

    monty

--- monty.h
+++ monty.h
@@ -23,7 +23,7 @@ typedef struct stack_s
 	int n;
 	struct stack_s *prev;
 	struct stack_s *next;
-} stack_t;
+} stack_t;stack_t;
 
 
 /**
@@ -38,4 +38,4 @@ typedef struct instruction_s
 {
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
-}
+
